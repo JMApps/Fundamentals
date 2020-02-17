28d0a6c2c263eae3e2b88998881b837a13af20d0
dev_muslim <jmappsmuslim@gmail.com>
2020-02-17 16:24:51 +0300

    Fixed package hierarchy

--- app/src/main/kotlin/jmapps/fundamentals/SplashActivity.kt
+++ app/src/main/kotlin/jmapps/fundamentals/SplashActivity.kt
@@ -3,6 +3,7 @@ package jmapps.fundamentals
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import jmapps.fundamentals.ui.activities.MainActivity
 
 class SplashActivity : Activity() {
 
