507f6e9f56d7c4fa18ccf088d7bad3cc33037f21
dev_muslim <jmappsmuslim@gmail.com>
2020-02-26 10:20:10 +0300

    Added scroll save method

---
+++ app/src/main/kotlin/jmapps/fundamentals/mvp/read/ScrollContract.kt
@@ -0,0 +1,20 @@
+package jmapps.fundamentals.mvp.read
+
+import android.content.SharedPreferences
+
+interface ScrollContract {
+
+    interface ScrollView {
+
+
+    }
+
+    interface ScrollPresenter {
+
+        fun scrollCount()
+
+        fun saveLastCount(editor: SharedPreferences.Editor)
+
+        fun loadLastCount(preferences: SharedPreferences)
+    }
+}
--- app/src/main/kotlin/jmapps/fundamentals/ui/fragment/PlaceholderFragment.kt
+++ app/src/main/kotlin/jmapps/fundamentals/ui/fragment/PlaceholderFragment.kt
@@ -11,18 +11,21 @@ import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import jmapps.fundamentals.R
 import jmapps.fundamentals.data.database.sqlite.lists.BookContentList
+import jmapps.fundamentals.mvp.read.ScrollContract
+import jmapps.fundamentals.mvp.read.ScrollPresenterImpl
 import jmapps.fundamentals.ui.model.BookContent
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
-class PlaceholderFragment : Fragment() {
+class PlaceholderFragment : Fragment(), ScrollContract.ScrollView {
 
-    private lateinit var numberOfPager: MutableList<BookContent>
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     private lateinit var rootMain: View
     private var sectionNumber: Int? = null
 
-    private lateinit var preferences: SharedPreferences
-    private lateinit var editor: SharedPreferences.Editor
+    private lateinit var numberOfPager: MutableList<BookContent>
+    private lateinit var scrollPresenterImpl: ScrollPresenterImpl
 
     companion object {
 
@@ -42,15 +45,24 @@ class PlaceholderFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootMain = inflater.inflate(R.layout.fragment_main, container, false)
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        editor = preferences.edit()
+
         sectionNumber = arguments?.getInt(ARG_SECTION_NUMBER)
         numberOfPager = BookContentList(context).getBookContentList
 
         rootMain.tvBookContentArabic.text = numberOfPager[sectionNumber!! - 1].contentArabic
         rootMain.tvBookContentTranslation.text = Html.fromHtml(numberOfPager[sectionNumber!! - 1].contentTranslation)
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        editor = preferences.edit()
+        scrollPresenterImpl = ScrollPresenterImpl(this, sectionNumber!!, rootMain.nsMainContainer, rootMain.lastReadingPosition)
+        scrollPresenterImpl.scrollCount()
+        scrollPresenterImpl.loadLastCount(preferences)
 
         return rootMain
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        scrollPresenterImpl.saveLastCount(editor)
     }
 }
