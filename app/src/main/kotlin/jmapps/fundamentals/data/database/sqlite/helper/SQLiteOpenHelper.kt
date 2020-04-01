c67c94e563fea01c15988a497061728c9aaf149b
dev_muslim <jmappsmuslim@gmail.com>
2020-04-01 15:53:14 +0300

    New database version

--- app/src/main/kotlin/jmapps/fundamentals/data/database/sqlite/helper/SQLiteOpenHelper.kt
+++ app/src/main/kotlin/jmapps/fundamentals/data/database/sqlite/helper/SQLiteOpenHelper.kt
@@ -3,7 +3,7 @@ package jmapps.fundamentals.data.database.sqlite.helper
 import android.content.Context
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
-private var DBVersion = 1
+private var DBVersion = 2
 
 class SQLiteOpenHelper(context: Context) :
     SQLiteAssetHelper(context, "Books", null, DBVersion) {
